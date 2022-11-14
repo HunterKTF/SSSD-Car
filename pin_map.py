01f2c6a0e004c383ed1764cab75f4add8e47cb27
Jorge Ricardo Hernández Sabino <40583956+HunterKTF@users.noreply.github.com>
2022-11-13 21:05:20 -0600

    Add files via upload

---
+++ pin_map.py
@@ -0,0 +1,39 @@
+'''
+PIN mapping using variables
+'''
+
+#  GPIO = Pin
+GPIO_2  = 3	# Used
+GPIO_3  = 5	# Used
+GPIO_4  = 7	# Used
+GPIO_5  = 29
+GPIO_6  = 31
+GPIO_7  = 26
+GPIO_8  = 24
+GPIO_9  = 21
+GPIO_10 = 19
+GPIO_11 = 23
+GPIO_12 = 32	# Used
+GPIO_13 = 33
+GPIO_14 = 8	# Used
+GPIO_15 = 10	#
+GPIO_16 = 36
+GPIO_17 = 11	# 
+GPIO_18 = 12	# Used
+GPIO_19 = 35
+GPIO_20 = 38
+GPIO_21 = 40
+GPIO_22 = 15	#
+GPIO_23 = 16	#
+GPIO_24 = 18
+GPIO_25 = 22
+GPIO_26 = 37
+GPIO_27 = 13
+
+# Funct = Pin
+ID_SD   = 27
+ID_SC   = 28
+PWM0_0  = 12
+PWM0_1  = 32
+PWM1_0  = 33
+PWM1_1  = 35
